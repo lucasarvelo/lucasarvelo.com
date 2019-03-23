39427c76bd24c8d446c20f0e2caea366cd937f78
Lucas Arvelo <lucasarvelo@gmail.com>
2019-03-22 21:41:52 -0400

    Added icons and style

--- src/components/WelcomeMenssage/WelcomeMenssage.jsx
+++ src/components/WelcomeMenssage/WelcomeMenssage.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-
+import "./WelcomeMenssage.scss";
 function WelcomeMenssage() {
   return (
-    <div className="p-3">
-      <p className="text-justify">
-        Hi, My name is Lucas, and I'm Ontario based Web Developer. I enjoy my
-        time building web apps with technologies like react, vue, redux, html,
-        css or javascript. <br />
-        <br />I love to solve problem and getting lost in rabit hole where I can
-        learn new skills.
-      </p>
+    <div className="col">
+      <div className="row">
+        <p className="text-justify font-weight-bold text-monospace welcome-msg">
+          Hi, My name is Lucas, and I'm Ontario based Web Developer. <br />I
+          enjoy my time building web apps with technologies like react, vue,
+          redux, html, css or javascript. <br />
+          <br />I love to solve problem and getting lost in rabit holes where I
+          can learn new skills.
+        </p>
+      </div>
+      <div className="row welcome-msg-icons d-flex justify-content-around">
+        <div>
+          <i className="fab fa-js-square m-3" />
+        </div>
+        <div>
+          <i className="fab fa-react m-3" />
+        </div>
+        <div>
+          <i className="fab fa-vuejs m3" />
+        </div>
+        <div>
+          <i className="fab fa-html5 m-3" />
+        </div>
+        <div>
+          <i className="fab fa-css3-alt m-3" />
+        </div>
+      </div>
     </div>
   );
 }
