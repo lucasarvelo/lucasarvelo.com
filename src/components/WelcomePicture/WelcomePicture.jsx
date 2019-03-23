542e967be295ea4687f92e43dddf08e9ea9d6423
Lucas Arvelo <lucasarvelo@gmail.com>
2019-03-22 21:42:04 -0400

    Style

--- src/components/WelcomePicture/WelcomePicture.jsx
+++ src/components/WelcomePicture/WelcomePicture.jsx
@@ -3,15 +3,13 @@ import mainImage from "../../assets/Lucas-min.jpg";
 
 function WelcomePicture() {
   return (
-    <div>
-      <img
-        src={mainImage}
-        alt="Lucas"
-        className="img-fluid rounded-lg float-left border align-middle"
-        height="200"
-        width="400"
-      />
-    </div>
+    <img
+      src={mainImage}
+      alt="Lucas"
+      className="img-fluid rounded-lg border p-1 mx-auto d-block shadow"
+      height="200"
+      width="400"
+    />
   );
 }
 
