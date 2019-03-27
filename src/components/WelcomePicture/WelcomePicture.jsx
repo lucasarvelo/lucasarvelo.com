61f763e546d75b85b3dfc5829a0da93cdb9b78be
Lucas Arvelo <lucasarvelo@gmail.com>
2019-03-26 23:00:35 -0400

    Change to arrow function component

--- src/components/WelcomePicture/WelcomePicture.jsx
+++ src/components/WelcomePicture/WelcomePicture.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import mainImage from "../../assets/Lucas-min.jpg";
 
-function WelcomePicture() {
+const WelcomePicture = () => {
   return (
-    <img
-      src={mainImage}
-      alt="Lucas"
-      className="img-fluid rounded-lg border p-1 mx-auto d-block shadow"
-      height="200"
-      width="400"
-    />
+    <div className="col-md-4 p-4">
+      <img
+        src={mainImage}
+        alt="Lucas"
+        className="img-fluid rounded-lg border p-1 mx-auto d-block shadow"
+        height="200"
+        width="400"
+      />
+    </div>
   );
-}
+};
 
 export default WelcomePicture;
--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default function Projects() {
+const Projects = () => {
   return <div />;
-}
+};
+
+export default Projects;
