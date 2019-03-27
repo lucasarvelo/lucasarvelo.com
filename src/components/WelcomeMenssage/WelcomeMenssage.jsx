08e51b59de91dd3347219f19a24b0a931c3cbec7
Lucas Arvelo <lucasarvelo@gmail.com>
2019-03-26 22:58:25 -0400

    Update + style

--- src/components/WelcomeMenssage/WelcomeMenssage.jsx
+++ src/components/WelcomeMenssage/WelcomeMenssage.jsx
@@ -1,36 +1,40 @@
 import React from "react";
 import "./WelcomeMenssage.scss";
-function WelcomeMenssage() {
+const WelcomeMenssage = () => {
   return (
-    <div className="col">
+    <div className="col-md-8 p-4">
       <div className="row">
-        <p className="text-justify font-weight-bold text-monospace welcome-msg">
-          Hi, My name is Lucas, and I'm Ontario based Web Developer. <br />I
-          enjoy my time building web apps with technologies like react, vue,
-          redux, html, css or javascript. <br />
+        <p className="text-center font-weight-bold text-monospace welcome-msg">
+          Hi, My name is Lucas, web developer based on Ontario. <br />I enjoy my
+          time building web apps with technologies like html, css or javascript
+          and tools like react, vue, redux, vuex, bootstrap, material, node,
+          express, mongodb or sass. <br />
           <br />I love to solve problem and getting lost in rabit holes where I
           can learn new skills.
         </p>
       </div>
       <div className="row welcome-msg-icons d-flex justify-content-around">
         <div>
-          <i className="fab fa-js-square m-3" />
+          <i className="fab fa-js-square" />
         </div>
         <div>
-          <i className="fab fa-react m-3" />
+          <i className="fab fa-react" />
         </div>
         <div>
-          <i className="fab fa-vuejs m3" />
+          <i className="fab fa-vuejs" />
         </div>
         <div>
-          <i className="fab fa-html5 m-3" />
+          <i className="fab fa-html5" />
         </div>
         <div>
-          <i className="fab fa-css3-alt m-3" />
+          <i className="fab fa-css3-alt" />
+        </div>
+        <div>
+          <i className="fab fa-bootstrap" />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default WelcomeMenssage;
